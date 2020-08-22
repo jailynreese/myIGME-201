119193b44dc6e47c41f075a5287dcfc4fa26881d
reeese <jreeset@gmail.com>
2020-08-22 19:05:29 -0400

    testing diff things

--- Talavera_HelloWorld/Program.cs
+++ Talavera_HelloWorld/Program.cs
@@ -10,7 +10,21 @@ namespace Talavera_HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! \n");
+
+            int age = 73;
+            int age2 = 34;
+            int age3 = age * age2;
+
+            Console.WriteLine(age + " * " + age2 + " = " + age3 + "\n");
+
+            string bff = "DONKEYY";
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Shrek yelled, \"" + bff + "!!!\" ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
