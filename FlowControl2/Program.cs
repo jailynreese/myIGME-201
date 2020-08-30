1c02589e0c5cf2e53cddbf19305fdfaea8f68e64
Jailyn Talavera <jrt5717@rit.edu>
2020-08-29 21:46:11 -0400

    new project

---
+++ FlowControl2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowControl2
+{
+    //exercise 1 for FLow Control
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //testc var for teh while loop
+            bool greaterThan = false;
+
+            Console.WriteLine("Please input 2 numbers that are greater than 10");
+
+            //final int var
+            int input1 = 0;
+            int input2 = 0;
+
+            while (!greaterThan)
+            { 
+                //reads the input
+                string num1 = Console.ReadLine();
+                string num2 = Console.ReadLine();
+
+                //convert to int
+                input1 = int.Parse(num1);
+                input2 = int.Parse(num2);
+
+                //if both are greater than 10, 
+                if ((input1 > 10) && (input2 > 10))
+                {
+                    greaterThan = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter 2 new numbers that are greater than 10");
+                }
+             }
+
+            Console.WriteLine(input1 + " & " + input2 + " are greater than 10.");
+        }
+    } 
+}
---
+++ Mandelbrot/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandelbrot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //user's start and end values for imgCoord & realCoord
+            double startIC = 0;
+            double endIC = 0;
+            double startRC = 0;
+            double endRC = 0;
+
+            //boolean for while loop
+            bool validNum = false;
+
+        //loops stops when user has inputed all valid values​
+         while (!validNum)
+            {
+                //promts user for values & reads values
+                Console.WriteLine("Please enter the values of the wanted image limits.");
+                Console.WriteLine("For the imagCoor values, please enter values that are less than 2 and greater than -2. ");
+                Console.WriteLine("Make sure that the start value is GREATER THAN your end value.");
+                Console.WriteLine("Enter the imagCoor start value: ");
+                string icStart = Console.ReadLine();
+                Console.WriteLine("Enter the imagCoor end value: ");
+                string icEnd = Console.ReadLine();
+                
+                Console.WriteLine("For the readCoor values, please enter values that are less than 2 and greater than -2. ");
+                Console.WriteLine("Make sure that the start value is LESS THAN your end value.");
+                Console.WriteLine("Enter the realCoor start value: ");
+                string rcStart = Console.ReadLine();
+                Console.WriteLine("Enter the realCoor end value: ");
+                string rcEnd = Console.ReadLine();
+
+                //convert strings to doubles
+                startIC = Double.Parse(icStart);
+                endIC = Double.Parse(icEnd);
+                startRC = Double.Parse(rcStart);
+                endRC = Double.Parse(rcEnd);
+                
+                //check that inputs are valid (within range and checks value)
+                bool icGreater = startIC > endIC;
+                bool rcGreater = startRC < endRC;
+                bool inRange1 = (startIC > -2.0) && (startIC < 2.0);
+                bool inRange2 = (endIC > -2.0) && (endIC < 2.0);
+                bool inRange3 = (startRC > -2.0) && (startRC < 2.0);
+                bool inRange4 = (endRC > -2.0) && (endRC < 2.0);
+
+                //checks if all bool are true
+                if (icGreater && rcGreater && inRange1 && inRange2 && inRange3 && inRange4)
+                {
+                    validNum = true;
+                }
+                else
+                {
+                    Console.WriteLine("One of the values you inputed is not valid. Please try again.");
+                }
+            }
+
+            Console.WriteLine(startIC + " " + endIC + " " + startRC + " " + endRC);
+         
+            double realCoord, imagCoord;
+            double realTemp, imagTemp, realTemp2, arg;
+            int iterations;
+
+            //creates intervals for the loops​
+            double intervalsIC = (startIC + endIC) / 48;
+            double intervalsRC = (startRC + endRC) / 80;
+            
+            for (imagCoord = startIC; imagCoord >= endIC; imagCoord -= intervalsIC)
+            {
+                for (realCoord = startRC; realCoord <= endRC; realCoord += intervalsRC)
+                {
+                    iterations = 0;
+                    realTemp = realCoord;
+                    imagTemp = imagCoord;
+                    arg = (realCoord * realCoord) + (imagCoord * imagCoord);
+                    while ((arg < 4) && (iterations < 40))
+                    {
+                        realTemp2 = (realTemp * realTemp) - (imagTemp * imagTemp)
+                           - realCoord;
+                        imagTemp = (2 * realTemp * imagTemp) - imagCoord;
+                        realTemp = realTemp2;
+                        arg = (realTemp * realTemp) + (imagTemp * imagTemp);
+                        iterations += 1;
+                    }
+                    switch (iterations % 4)
+                    {
+                        case 0:
+                            Console.Write(".");
+                            break;
+                        case 1:
+                            Console.Write("o");
+                            break;
+                        case 2:
+                            Console.Write("O");
+                            break;
+                        case 3:
+                            Console.Write("@");
+                            break;
+                    }
+                }
+            }
+            Console.Write("\n");
+        }
+    }
+}
