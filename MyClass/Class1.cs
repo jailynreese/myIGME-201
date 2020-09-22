f4d12d6df2b0f676a7c637f5451c832f4d6d79cb
jrt5717 <jrt5717@IGM2000-04>
2020-09-22 17:26:18 -0400

    added myDerivedClass

---
+++ MyClass/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass
+{
+    public class Class1
+    {
+        private string myString;
+
+        public string MyString
+        {
+            get 
+            {
+                return null;
+            }
+            set
+            {
+                myString = value;
+            }
+               
+        }
+        public virtual string GetString()
+        {
+
+          return myString; 
+
+        }
+
+    }
+}
---
+++ MyDerivedClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClass;
+
+namespace MyDerivedClass
+{
+    // Class Program
+    // Author: Jailyn Talavera
+    // Purpose: PE12 Defining Class Members
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: solution to #3
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            MyDerivedClass strings = new MyDerivedClass();
+            string output = strings.GetString();
+            Console.WriteLine(output);
+        }
+    }
+
+    //inherits Class1 (MyClass)
+    class MyDerivedClass : Class1
+    {
+
+        //overring getString
+        public override string GetString()
+        {
+            //returning string from base + text
+            return base.GetString() + " (output from the derived class)";
+        }
+    }
+}
